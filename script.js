964e921ac929ef3d5c075904b392ffec25bedbc2
JesseJayR7 <76219568+JesseJayR7@users.noreply.github.com>
2021-08-20 05:01:07 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let answers = ['spongebob squarepants', 'bikini bottom', 'patrick star', 'krusty krabs'];
+let answer = document.getElementById('answer');
+let score = document.getElementById('score');
+let image = document.getElementById('spongebob');
+let button = document.getElementById('submit');
+score.innerHTML = `<h1>0</h1>`;
+image.innerHTML = `<img src="SPONGEBOB.png" alt="SpongeBob-Image"/>`;
